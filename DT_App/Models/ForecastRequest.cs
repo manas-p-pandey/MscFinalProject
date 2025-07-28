256744ef418b709fdcb651d6f30b173fcdb9d948
Manas Prasun Pandey <pandeym@roehampton.ac.uk>
2025-07-28 05:29:46 +0100

    models update

---
+++ DT_App/Models/ForecastRequest.cs
@@ -0,0 +1,13 @@
+﻿using DT_App.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+public class ForecastRequest
+{
+    [JsonProperty("datetime")]
+    public DateTime DateTime { get; set; }
+
+    [JsonProperty("traffic_data")]
+    public List<TrafficData> TrafficData { get; set; }
+}
---
+++ DT_App/Models/TrafficData.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+public class TrafficData
+{
+    [JsonProperty("latitude")]
+    public double Latitude { get; set; }
+
+    [JsonProperty("longitude")]
+    public double Longitude { get; set; }
+
+    [JsonProperty("traffic_density")]
+    public string TrafficDensity { get; set; }
+}
